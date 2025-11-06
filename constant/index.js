0ad0f46f820e18ea62e5f7c8381549d315ade058
nurarifahr <202030354@mhs.udb.ac.id>
2025-11-06 10:42:05 +0700

    initial commit

---
+++ constant/index.js
@@ -0,0 +1,131 @@
+const contentSatu = [
+    {
+        id: 1,
+        colorBorder: "border-blue-facebook",
+        logo: "images/icon-facebook.svg",
+        username: "@nathanf",
+        jumlah: "1987",
+        ket: "FOLLOWERS",
+        icon: "/images/icon-up.svg",
+        ketIcon: "12 Today",
+        colorKetIcon: "text-pgreen-500",
+
+    },
+    {
+        id: 2,
+        colorBorder: "border-blue-twitter",
+        logo: "images/icon-twitter.svg",
+        username: "@nathanf",
+        jumlah: "1044",
+        ket: "FOLLOWERS",
+        icon: "/images/icon-up.svg",
+        ketIcon: "99 Today",
+        colorKetIcon: "text-pgreen-500",
+
+    },
+    {
+        id: 3,
+        colorBorder: "border-image-instagram",
+        logo: "images/icon-instagram.svg",
+        username: "@realnathanf",
+        jumlah: "11K",
+        ket: "FOLLOWERS",
+        icon: "/images/icon-up.svg",
+        ketIcon: "1099 Today",
+        colorKetIcon: "text-pgreen-500",
+
+    },
+    {
+        id: 4,
+        colorBorder: "border-red-youtube",
+        logo: "images/icon-youtube.svg",
+        username: "Nathan F.",
+        jumlah: "8239",
+        ket: "SUBSCRIBERS",
+        icon: "/images/icon-down.svg",
+        ketIcon: "144 Today",
+        colorKetIcon: "text-pred-500",
+
+    },
+];
+
+const contentDua = [
+    {
+        id: 1,
+        title: "Page Views",
+        logo: "/images/icon-facebook.svg",
+        jumlah: "87",
+        icon: "/images/icon-up.svg",
+        colorKetIcon: "text-pgreen-500",
+        ket: "3%",
+    },
+    {
+        id: 2,
+        title: "Likes",
+        logo: "/images/icon-facebook.svg",
+        jumlah: "52",
+        icon: "/images/icon-down.svg",
+        colorKetIcon: "text-pred-500",
+        ket: "2%",
+    },
+    {
+        id: 3,
+        title: "Likes",
+        logo: "/images/icon-instagram.svg",
+        jumlah: "5462",
+        icon: "/images/icon-up.svg",
+        colorKetIcon: "text-pgreen-500",
+        ket: "2257%"
+    },
+    {
+        id: 4,
+        title: "Profile Views",
+        logo: "/images/icon-instagram.svg",
+        jumlah: "52K",
+        icon: "/images/icon-up.svg",
+        colorKetIcon: "text-pgreen-500",
+        ket: "1375%"
+    },
+    {
+        id: 5,
+        title: "Retweets",
+        logo: "/images/icon-twitter.svg",
+        jumlah: "117",
+        icon: "/images/icon-up.svg",
+        colorKetIcon: "text-pgreen-500",
+        ket: "303%"
+    },
+    {
+        id: 6,
+        title: "Likes",
+        logo: "/images/icon-twitter.svg",
+        jumlah: "507",
+        icon: "/images/icon-up.svg",
+        colorKetIcon: "text-pgreen-500",
+        ket: "553%"
+    },
+    {
+        id: 7,
+        title: "Likes",
+        logo: "/images/icon-youtube.svg",
+        jumlah: "107",
+        icon: "/images/icon-down.svg",
+        colorKetIcon: "text-pred-500",
+        ket: "19%"
+    },
+    {
+        id: 8,
+        title: "Total Views",
+        logo: "/images/icon-youtube.svg",
+        jumlah: "1407",
+        icon: "/images/icon-down.svg",
+        colorKetIcon: "text-pred-500",
+        ket: "12%"
+    },
+    
+];
+
+export{
+    contentSatu,
+    contentDua,
+}
